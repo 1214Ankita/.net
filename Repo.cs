dccbff4cf20699fe55901625ff18fd381f932e4d
AnkCreates <118843633+1214Ankita@users.noreply.github.com>
2025-03-02 19:02:54 +0530

    Update Repo.cs

--- Repo.cs
+++ Repo.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using DishApi.Models;
-using Newtonsoft.Json;
 
 namespace DishApi.Repositories
 {
@@ -15,37 +14,10 @@ namespace DishApi.Repositories
 
         public ExcelDishRepository()
         {
-            // Ensure the file exists and has some sample data
+            // Ensure the file exists
             if (!File.Exists(_filePath))
             {
-                CreateSampleData();
-            }
-        }
-
-        private void CreateSampleData()
-        {
-            using (var package = new ExcelPackage())
-            {
-                var worksheet = package.Workbook.Worksheets.Add("Dishes");
-                worksheet.Cells[1, 1].Value = "Id";
-                worksheet.Cells[1, 2].Value = "Name";
-                worksheet.Cells[1, 3].Value = "Description";
-                worksheet.Cells[1, 4].Value = "Price";
-                worksheet.Cells[1, 5].Value = "Type"; // New column for type
-
-                worksheet.Cells[2, 1].Value = 1;
-                worksheet.Cells[2, 2].Value = "Spaghetti";
-                worksheet.Cells[2, 3].Value = "Delicious spaghetti with marinara sauce.";
-                worksheet.Cells[2, 4].Value = 12.99;
-                worksheet.Cells[2, 5].Value = DishType.MainDish.ToString(); // Type
-
-                worksheet.Cells[3, 1].Value = 2;
-                worksheet.Cells[3, 2].Value = "Chocolate Cake";
-                worksheet.Cells[3, 3].Value = "Rich chocolate cake with frosting.";
-                worksheet.Cells[3, 4].Value = 5.99;
-                worksheet.Cells[3, 5].Value = DishType.Dessert.ToString(); // Type
-
-                package.SaveAs(new FileInfo(_filePath));
+                throw new FileNotFoundException("The Excel file does not exist.", _filePath);
             }
         }
 
@@ -59,20 +31,110 @@ namespace DishApi.Repositories
 
                 for (int row = 2; row <= rowCount; row++)
                 {
-                    var dishType = (DishType)Enum.Parse(typeof(DishType), worksheet.Cells[row, 5].Value.ToString());
-                    var dish = new Dish
+                    var dishType = worksheet.Cells[row, 5].Value.ToString();
+                    Dish dish = dishType switch
                     {
-                        Id = (int)worksheet.Cells[row, 1].Value,
-                        Name = worksheet.Cells[row, 2].Value.ToString(),
-                        Description = worksheet.Cells[row, 3].Value.ToString(),
-                        Price = (decimal)worksheet.Cells[row, 4].Value,
-                        Type = dishType // Set the type
+                        "MainDish" => new MainDish
+                        {
+                            Id = (int)worksheet.Cells[row, 1].Value,
+                            Name = worksheet.Cells[row, 2].Value.ToString(),
+                            Description = worksheet.Cells[row, 3].Value.ToString(),
+                            Price = (decimal)worksheet.Cells[row, 4].Value,
+                            MainIngredient = "Pasta" // Example main ingredient
+                        },
+                        "Dessert" => new Dessert
+                        {
+                            Id = (int)worksheet.Cells[row, 1].Value,
+                            Name = worksheet.Cells[row, 2].Value.ToString(),
+                            Description = worksheet.Cells[row, 3].Value.ToString(),
+                            Price = (decimal)worksheet.Cells[row, 4].Value,
+                            IsGlutenFree = true // Example property
+                        },
+                        _ => null // Skip unknown types
                     };
 
-                    dishes.Add(dish);
+                    if (dish != null)
+                    {
+                        dishes.Add(dish);
+                    }
                 }
             }
             return await Task.FromResult(dishes);
+        }
+
+        public async Task<Dish> GetDishByIdAsync(int id)
+        {
+            var dishes = await GetAllDishesAsync();
+            return dishes.FirstOrDefault(d => d.Id == id);
+        }
+
+        public async Task AddDishAsync(Dish dish)
+        {
+            var dishes = await GetAllDishesAsync();
+            dish.Id = dishes.Count > 0 ? dishes.Max(d => d.Id) + 1 : 1;
+
+            using (var package = new ExcelPackage(new FileInfo(_filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+                var rowCount = worksheet.Dimension.Rows;
+
+                worksheet.Cells[rowCount + 1, 1].Value = dish.Id;
+                worksheet.Cells[rowCount + 1, 2].Value = dish.Name;
+                worksheet.Cells[rowCount + 1, 3].Value = dish.Description;
+                worksheet.Cells[rowCount + 1, 4].Value = dish.Price;
+                worksheet.Cells[rowCount + 1, 5].Value = dish.GetDishType(); // Set the type
+
+                package.Save();
+            }
+        }
+
+        public async Task UpdateDishAsync(Dish dish)
+        {
+            var dishes = await GetAllDishesAsync();
+            if (dishes.Any(d => d.Id == dish.Id))
+            {
+                using (var package = new ExcelPackage(new FileInfo(_filePath)))
+                {
+                    var worksheet = package.Workbook.Worksheets[0];
+                    var rowCount = worksheet.Dimension.Rows;
+
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        if ((int)worksheet.Cells[row, 1].Value == dish.Id)
+                        {
+                            worksheet.Cells[row, 2].Value = dish.Name;
+                            worksheet.Cells[row, 3].Value = dish.Description;
+                            worksheet.Cells[row, 4].Value = dish.Price;
+                            worksheet.Cells[row, 5].Value = dish.GetDishType(); // Update the type
+                            break;
+                        }
+                    }
+                    package.Save();
+                }
+            }
+        }
+
+        public async Task DeleteDishAsync(int id)
+        {
+            var dishes = await GetAllDishesAsync();
+            if (dishes.Any(d => d.Id == id))
+            {
+                using (var package = new ExcelPackage(new FileInfo(_filePath)))
+                {
+                    var worksheet = package.Workbook.Worksheets[0];
+                    var rowCount = worksheet.Dimension.Rows;
+
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        if ((int)worksheet.Cells[row, 1].Value == id)
+                        {
+                            worksheet.DeleteRow(row);
+                            break;
+                        }
+                    }
+                    package.Save();
+                }
+            }
         }
     }
 }
